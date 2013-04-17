fac83a58f388d61a81b110446f0673aaff0bd7b7
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-17 18:14:16 -0400

    defining varables screen cast

--- linking js to html/js/script.js
+++ linking js to html/js/script.js
@@ -1,9 +1,9 @@
-// JavaScript
+// Defining Variables
 
-var yearBorn = 1983;
-/*
-console.log('Hello World!');
-console.log (yearBorn);
-    */
 
-alert(yearBorn);
+var age; //declaration
+age = 38;  // definition
+
+//storage container = value;
+
+var yearOfBirth = 1983;  //declaring and defining on the same line
