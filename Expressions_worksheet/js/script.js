3fa67f2b4756f6ca9c112f74aa36a54365cc6ae2
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 00:55:29 -0400

    Dog Years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,9 @@
+//   Newland_Bobby _ 4/17/13  Expression Work Sheet
+
+
+
+  var sparkysAgeInHumanYears = 2;   //  human years
+
+  var sparkysAgeInDogYears = 7;     //  dog years
+var sparkysTrueAge = sparkysAgeInHumanYears * sparkysAgeInDogYears;  //Arithmetic
+console.log(sparkysTrueAge);  // Sparky's age in dog years
