ef871b6e4cec61ca0e7b52c13c75009f49c7327f
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 19:49:02 -0400

    the problem

--- Week 2 Assignment/Feed The Family /js/script.js
+++ Week 2 Assignment/Feed The Family /js/script.js
@@ -1,3 +1,9 @@
 //   Newland_Bobby _ 4/18/13  How many eggs will it take to feed a family of 3 breakfast all week if they each eat 2 eggs
 
 
+/* mom dad and kid will eat 6 eggs a day
+                         x  7 days a week
+                          ______
+ */
+
+
