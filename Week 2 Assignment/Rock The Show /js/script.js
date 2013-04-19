10c38ae4eee61db46cf8e9931dec0b117ddf152c
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 20:32:00 -0400

    the answer

--- Week 2 Assignment/Rock The Show /js/script.js
+++ Week 2 Assignment/Rock The Show /js/script.js
@@ -1,1 +1,7 @@
 //   Newland_Bobby _ 4/18/13  How much money will we make after expense if we sell all the tickets to the show?
+
+
+/* For the show we will need a venue for $500, a dj for $250, and a special guest for $250.
+ We will sell the tickets for $30 each.
+ We have 200 tickets
+
