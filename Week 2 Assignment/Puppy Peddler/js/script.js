df23389944fd65f1166ec0ac519a155ac112f52f
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 21:02:22 -0400

    the code : console.log;

--- Week 2 Assignment/Puppy Peddler/js/script.js
+++ Week 2 Assignment/Puppy Peddler/js/script.js
@@ -23,3 +23,4 @@ var dog = [litter1, litter2, litter3];   // puppy array will be indexes 0,1,2
 
 var total =  [(dog[2]) - sale - give + (dog[1]) -sale -give + (dog[0]) - sale - give];
 
+console.log(total);
