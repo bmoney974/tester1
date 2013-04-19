c4c05a3fa6df9f1c5a3f2db10bbc50ec695cd461
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 21:25:49 -0400

    the code: console.log final final

--- Week 2 Assignment/Rock The Show /js/script.js
+++ Week 2 Assignment/Rock The Show /js/script.js
@@ -15,9 +15,11 @@ var venue = 500;  // total expenses
 var dj = 250;
 var specialGuest = 250;
 
+var name = "After expenses the amount of money left will be"
+
 var cost = [venue, dj, specialGuest];   // cost array will be indexes 0,1,2
 
-var total = tickets * 200 - [(cost[2]) + (cost[1]) + (cost[0])];
+var total = name + " " + ( tickets * 200 - [(cost[2]) + (cost[1]) + (cost[0])] );
 
 console.log(total);
 
