e9535b05e88b4b3e99958455a70616c90b74fe12
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 21:00:04 -0400

    the Problem

--- Week 2 Assignment/Puppy Peddler/js/script.js
+++ Week 2 Assignment/Puppy Peddler/js/script.js
@@ -2,3 +2,13 @@
 // how many puppies will be left
 
 
+
+/*  3 dogs x 6 puppies = 18 puppies
+ litter 1 - 6 puppies - 2 sale - 2 give a way = 2 puppies left
+ litter 2 - 6 puppies - 2 sale - 2 give away = 2 puppies left
+ litter 3 - 6 puppies - 2 sale - 2 give away = 2 puppies left
+
+ total of 6 puppies left
+ */
+
+
