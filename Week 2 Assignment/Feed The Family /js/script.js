c6f98a136495d58c572fbac70c1d599e2884b161
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 21:27:51 -0400

    the code: console.log final final

--- Week 2 Assignment/Feed The Family /js/script.js
+++ Week 2 Assignment/Feed The Family /js/script.js
@@ -13,9 +13,11 @@ var mom = 2;  // each person will eat 2 eggs
 var dad = 2;
 var kid = 2;
 
+var name = "The total number of eggs for the week will be"
+
 var daysInWeek = 7; // number of days in a week
 var family = [mom, dad, kid]; // family array will be indexes 0,1,2
-var total =  [ (family[0]) +(family[1]) + (family[2]) ] * eggs * daysInWeek;
+var total =  name + " " + [ (family[0]) +(family[1]) + (family[2]) ] * eggs * daysInWeek;
 
 
 
