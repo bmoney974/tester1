3ea9b011c633ee99772eafdaf66e73284a2213bd
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-18 22:02:47 -0400

    groceries

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,13 +2,21 @@
 
 
 
-var slices = 8;
-var people = 20;
-var pizzas = 5;
-var name = " Sparky got"
-var name2 = " slices of pizza"
-var sparkysPizza = 0
+var shop1 = 50
+var shop2 = 65
+var shop3 = 40
+var shop4 = 70
+var shop5 = 30
 
+var name = "We spent a total of "
+var name2 = "on groceries that's an average of"
+var name3 = "a week"
+var name4 = "$"
 
-var total = name + " " + (slices * pizzas ) /10 * sparkysPizza + " " + name2;
-console.log(total);
+var weekly = [shop1, shop2, shop3, shop4, shop5]; // 0,1,2,3,4
+
+var total = (weekly[0]) + (weekly[1]) + (weekly[2]) + (weekly[3]) + (weekly[4]);
+
+var total2 = name + " " + name4 + total   + " " + name2 + " "+ name4 + (total/5) + " " + name3;
+
+console.log(total2);
