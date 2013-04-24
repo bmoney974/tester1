3f68757b563b34c7d9e30166a0d84e2e8cca91d6
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-24 14:47:09 -0400

    logical operators screencast ||

--- linking js to html/js/script.js
+++ linking js to html/js/script.js
@@ -1,10 +1,10 @@
 // Conditional Logic -- Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200
+var wonLottery = true;
 // if the price of th e phone is less than our budget and if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery){
     console.log("We Can Buy The Phone!");
 }else{
     console.log("No Phone For You!!!")
