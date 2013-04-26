7915529053ba9c2226472d517e0f1c918be9b091
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-04-25 21:49:58 -0400

    if else 1

--- Conditional_personal/js/script.js
+++ Conditional_personal/js/script.js
@@ -6,4 +6,5 @@ var check = prompt(" How Much is your check ");  // how much is check
 
 if(check >= 400  ){       // if check is greater than or equal to 400
     console.log(" You can get an out fit and go to the movies")  // answer 1
-
+} else if(check < 400 && check >= 300 ){    // if check is less than 400 and greater than or equal to 300
+    console.log("You can only go to the moves")     // answer 2
