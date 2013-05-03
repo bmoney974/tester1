429127343498cfc4f927caa7eb6a71a337ee274c
Bobby Newland <bobbynewland@Bobbys-MacBook-Pro.local>
2013-05-02 21:14:05 -0400

    functions industry with comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,20 +1,20 @@
 // Bobby Newland Assignment Week 3
 // if we sell x amount of cds each week we will be able to buy a van at the end of the month after expenses
 
-var week1 = prompt ("Week 1 Number of Cds sold");
+var week1 = prompt ("Week 1 Number of Cds sold");     // prompts
 var week2 = prompt ("Week 2 Number of Cds sold");
 var week3 = prompt ("Week 2 number of Cds Sold");
 
-function buyTheVan(vanCost){
+function buyTheVan(vanCost){    // function
     var cdValue = 10
     var expenses = 1000
 
-
+                                            // variables
     var a = parseInt(week1);
     var b = parseInt(week2);
     var c = parseInt(week3);
 
-    var totalSales = (a + b + c) * cdValue;
+    var totalSales = (a + b + c) * cdValue;     // total sales calculation
     var profit = (totalSales - expenses );
 
     var verdict
@@ -24,7 +24,7 @@ function buyTheVan(vanCost){
     console.log(verdict);
 }
 
-buyTheVan(3000);
+buyTheVan(3000);    // function call
 
 
 
